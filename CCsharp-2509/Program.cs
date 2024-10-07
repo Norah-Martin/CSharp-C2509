a86499474672c2bcadbf7c8d48d081748b0f4885
norah <norahmartin15@gmail.com>
2024-10-07 09:40:52 +0530

    sample

---
+++ CCsharp-2509/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCsharp_2509
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // int num = 5;
+            //  double dnum = 5.90;
+            // char let = 'a';
+
+            // bool mybool= false;
+
+            // string test = "hi";
+
+            // test = test + "hello";
+
+            // Console.WriteLine(test);
+
+            // Console.ReadLine();
+
+            //int a = 5;int b = 6;int c = 7;
+            //int d;
+            //d=a+b+c;
+            //Console.WriteLine(d);
+            //Console.ReadLine();
+
+            //OPERATORS
+            //arithmetic,assignment
+
+            //&&-Logical and
+            //||-Logical or
+            //!-Logicalnot
+
+            //int x = 25;
+            //int y = 10;
+            //int z = 12;
+            //if ((x > y) && (x > z))
+            //{
+
+            //    Console.WriteLine("x is greater");
+            //}
+        }
+    }
+}
