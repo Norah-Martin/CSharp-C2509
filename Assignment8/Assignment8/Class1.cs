3307ac2450c05755c03ce5883a5e8950458b9def
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-10-25 15:43:13 +0530

    Assigment8
    OOPS-Inheritance Assignment

---
+++ Assignment8/Assignment8/Class1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+   public class Class1
+    {
+        //  1      Create a base class Person with properties like Name and Age.Derive a class Student from
+        //Person and add a property Grade.Create objects and display the data.
+
+        //public class Person
+        //{
+        //    public string Name;
+        //    public int age;
+        //    public void display()
+        //    {
+        //        Console.WriteLine($"{Name}----------{age}");
+        //    }
+        //}
+        //public class Student : Person
+        //{
+        //    public string grade;
+        //    public void displays()
+        //    {
+        //        Console.WriteLine($"{Name}-----{age}------{grade}");
+        //    }
+        //}
+
+
+        //2  multilevel   .Create a base class Animal with a method Eat(). Derive a class Dog that inherits Animal and add
+        // a method Bark(). Further derive a class Puppy from Dog and add a method Weep(). 
+
+        //public class Animal
+        //{
+        //    public void Eat()
+        //    {
+        //        Console.WriteLine("---Eating----");
+        //    }
+
+        //}
+        //public class Dog:Animal
+        //{
+        //    public void Bark()
+        //    {
+        //        Console.WriteLine("----Barking----");
+        //    }
+        //}
+        //public class Puppy:Dog
+        //{
+        //    public void Weep()
+        //    {
+        //        Console.WriteLine("----Weeping-----");
+        //    }
+        //}
+
+
+        //3.multiple via interface
+        //Create two interfaces IPrintable and IScannable with respective methods Print() and Scan().
+        //Implement both interfaces in a class PrinterScanner.
+
+        //interface IPrintable
+        //{
+        //     void Print();
+        //}
+        //interface IScannable
+        //{
+        //    void Scan();
+        //}
+        //public class PrintScanner:IPrintable,IScannable
+        //{
+        //    public void Scan()
+        //    {
+        //        Console.WriteLine("--SCAN---");
+        //    }
+
+        //    public void Print()
+        //    {
+        //        Console.WriteLine("--PRINT---");
+        //    }
+        //}
+
+
+        //        //4.hierarchial 
+        //        Create a base class Shape with a method Draw(). Create two derived classes Circle and
+        //Rectangle, both inheriting from Shape, and override the Draw() method to show specific
+        //behavior.
+        //public class Shape
+        //{
+        //    public virtual void Draw(int sides)
+        //    {
+        //        Console.WriteLine($"sides is :{sides}");
+        //    }
+        //}
+        //public class Circle:Shape
+        //{
+        //    public   override void Draw(int sides)
+        //    {
+        //        Console.WriteLine($"Circle and sides is:{sides}");
+        //    }
+        //}
+        //public class Rectangle : Shape
+        //{
+        //    public  override  void Draw(int sides)
+        //    {
+        //        Console.WriteLine($"Reactangle and side is:{sides}");
+
+        //    }
+        //}
+
+
+        //5.Hybrid Inheritnce
+        //Create two interfaces IMovable and IRechargeable.Create a class Vehicle implementing
+        //IMovable and derive ElectricCar from Vehicle implementing both interfaces.
+
+        //interface IMovable
+        //{
+        //    void add(int a, int b);
+        //}
+        //interface IRechargeable
+        //{
+        //    void sub(int a, int b);
+
+        //}
+        //public class Vehicle:IMovable,IRechargeable
+        //{
+        //    public void add(int a,int b)
+        //    {
+        //        int sum = a + b;
+        //        Console.WriteLine($"sum is{sum}");
+        //    }
+        //    public void sub(int a,int b)
+        //    {
+        //        int diff = a - b;
+        //        Console.WriteLine($"diff is{diff}");
+        //    }
+        //}
+
+        //public class ElectricCar : Vehicle
+        //{
+        //    public void addsub()
+        //    {
+        //        Console.WriteLine("--addsub---");
+        //    }
+        //}
+
+
+        //6.  overriden
+        //Create a class Employee with a method Work(). Derive a class Manager that overrides the
+        //Work() method to show a different implementation.
+
+        //public class Employee
+        //{
+        //    public virtual void Work()
+        //    {
+        //        Console.WriteLine("--employee work--");
+        //    }
+        //}
+
+        //public class Manager:Employee
+        //{
+        //    public override void Work()
+        //    {
+        //        Console.WriteLine("--Manager work--");
+        //    }
+        //}
+
+        //7.abstract class
+        //Create an abstract class Vehicle with an abstract method Drive(). Create two derived classes Car
+        // and Bike that implement the Drive() method.
+
+        //public abstract class Vehicle
+        //{
+        //    public abstract void Drive();
+
+        //}
+        //public class Car:Vehicle
+        //{
+        //    public override void Drive()
+        //    {
+        //        Console.WriteLine("--Car is driving--");
+        //    }
+
+        //}
+        //public class Bike : Vehicle
+        //{
+        //    public override void Drive()
+        //    {
+        //        Console.WriteLine("--Bike is driving--");
+        //    }
+        //}
+
+        //8.sealed
+        // Create a sealed class MathOperations with a method Add(). Show that it cannot be inherited.
+
+        //public sealed class MathOperations
+        //{
+        //    public void Add(int a,int b)
+        //    {
+        //        int sum = a + b;
+        //        Console.WriteLine($"sum is:{sum}");
+        //    }
+        //}
+        //public class Math:MathOperations
+
+        //{
+        //    public void print()
+        //    {
+        //        Console.WriteLine("--------------------");
+        //    }
+        //}
+
+        //9  CONSTRUCTOR CHAINING
+        //Create a base class Person with a parameterized constructor.Create a derived class Employee
+        //that calls the base class constructor. 
+
+        //public class Person
+        //{
+        //    public Person(string name)
+        //    {
+        //        Console.WriteLine($"name is {name}");
+        //    }
+        //}
+        //public class Employee : Person
+        //{
+        //    public Employee(string name) : base(name)
+        //    {
+        //        Console.WriteLine($"employee name:{name}");
+        //    }
+        //}
+
+        //10 interface inheritance
+
+        //Create an interface IDriveable with a method Drive(). Create another interface IRaceable that
+        //inherits from IDriveable and adds a method Race(). 
+
+        //interface IDriveable
+        //{
+        //    void Drive();
+        //}
+        //interface Iraceable:IDriveable
+        //{
+        //    void race();
+        //}
+        //public class A:IDriveable,Iraceable
+        //{
+        //    public void Drive()
+        //    {
+        //        Console.WriteLine("--Drive--");
+        //    }
+
+        //    public void race()
+        //    {
+        //        Console.WriteLine("--Race--");
+        //    }
+        //}
+
+        //11 Is-a reltionshp
+        //Create a base class Animal with properties like Name and methods like Eat(). Create a derived class
+        //Dog that inherits from Animal and adds its own method Bark(). Show how the IS-A relationship
+        //works.
+
+        //public class Animal
+        //{
+        //    public string Name;
+        //    public void Eat()
+        //    {
+        //        Console.WriteLine($"{Name} is eating");
+        //    }
+        //}
+        //public class Dog : Animal
+        //{
+        //    public void Bark()
+        //    {
+        //        Console.WriteLine("Dog is Barking");
+        //    }
+        //}
+
+        //12 has 
+        //.Create a class Engine with properties like HorsePower. Create a class Car that contains an instance
+        //of Engine and shows the HAS-A relationship
+
+        //public class Engine
+        //{
+        //    public Engine(int HorsePower,string name)
+        //    {
+        //        Console.WriteLine($"{name} has a Horsepower {HorsePower}");
+        //    }
+        //}
+        //public class  Car:Engine
+        //{
+        //    public  Car(string carname,int HorsePower,string name): base(HorsePower, name)
+        //    {
+        //        Console.WriteLine($"{carname} has {name} engine with HorsePower {HorsePower}");
+        //    }
+        //}
+
+        //13 calling base class method using base
+        //Create a base class Person with a method DisplayInfo(). Derive a class Employee that overrides
+        //DisplayInfo() but still calls the base class's DisplayInfo() using base. 
+        //public class Person
+        //{
+        //    public string Name { get; set; }
+        //    public string Job {  get; set; }
+        //    public Person(string name, string job)
+        //    {
+        //        Name = name;
+        //        Job = job;
+
+        //    }
+        //    public virtual void DisplayInfo()
+        //    {
+        //        Console.WriteLine($"{Name} is a {Job}");
+        //    }
+
+        //}
+        //public class  Employee:Person
+        //{
+        //    public int Age {  get; set; }
+        //    public Employee(string name,string job,int age): base( name,job)
+        //    {
+        //        Age = age;
+        //    }
+        //    public override void DisplayInfo()
+        //    {
+        //        base.DisplayInfo();
+        //        Console.WriteLine($"{Name} is a {Job} and Age is  {Age}");
+
+        //    }
+        //}
+
+        //14.Create a base class Person with a property Name. In the derived class Student, hide the Name
+        // property using the new keyword and use base to access the base class's Name property. 
+
+        //public class Person
+        //{
+        //    public string Name { get; set; }
+        //    public Person(string name)
+        //    {
+        //        this.Name = name;
+        //        Console.WriteLine($"base class name:{Name}");
+        //    }
+        //}
+        //public class Student:Person
+        //{
+        //    public new string Name { get; set; }
+        //    public Student(string name) : base(name)
+        //    {
+        //       this. Name = name;
+        //        Console.WriteLine($"Derived class name:{this.Name}");
+        //    }
+
+        //}
+
+        //15.   Create a base class Vehicle with a constructor that accepts brand.Derive a class Car that passes
+        //values to the base class constructor using base. 
+
+        public class Vehicle
+        {
+            public string Brand { get; set; }
+            public Vehicle(string brand)
+            {
+                Brand = brand;
+                Console.WriteLine($"Vehicle brand is:{Brand}");
+            }
+        }
+        public class Car:Vehicle
+        {  
+            
+            public Car(string brand):base(brand)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Assignment8/Assignment8/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assignment8.Class1;
+
+namespace Assignment8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////single level
+            //Person p = new Person();
+            //p.Name = "norah";
+            //p.age = 23;
+            //p.display();
+
+            //Student s = new Student();
+            //s.grade = "A";
+            //Console.WriteLine("\n");
+            //Console.WriteLine("--------------------------");
+            //s.Name = "MARTIN";
+            //s.age = 21;
+            //s.display();
+            //s.displays();
+
+
+            //2.multilevel
+
+            //Animal A= new Animal();
+            //A.Eat();
+            //Console.WriteLine("\n");
+
+            //Dog D = new Dog();
+            //D.Eat();
+            //D.Bark(); 
+
+            //Console.WriteLine("\n");
+            //Puppy P = new Puppy();
+            //P.Eat();
+            //P.Bark();
+            //P.Weep();
+
+            //3. MULTIPLE
+
+            //PrintScanner P=new PrintScanner();
+            //P.Scan();
+            //P.Print();
+
+            ////4. override
+            ///
+            //Shape shape = new Shape();
+            //shape.Draw(3);
+
+            //Circle c= new Circle();
+            //c.Draw(4);
+
+            //Rectangle r= new Rectangle();
+            //r.Draw(6);
+
+
+            //5.interfcae
+
+            //Vehicle v = new Vehicle();
+            //v.add(1, 2);
+            //v.sub(4, 3);
+            //Console.WriteLine("\n");
+
+            //ElectricCar e=new ElectricCar();
+            //e.add(5, 6);
+            //e.sub(5, 3);
+            //e.addsub();
+
+
+            //6.override
+
+            //Employee e=new Employee();
+            //e.Work();
+            //Console.WriteLine("\n");
+
+            //Manager m = new Manager();
+            //m.Work();
+
+            //7.abstract
+            //Car c = new Car();
+            //c.Drive();
+
+            //Console.WriteLine("\n");
+            //Bike b = new Bike();
+            //b.Drive(); 
+
+            //8 sealed class
+            //MathOperations m=new MathOperations();
+            //m.Add(2, 3);
+
+            //9. constructor chaining
+
+            //Person p = new Person("norah");
+            //Console.WriteLine("\n");
+            //Employee e = new Employee("MARTIN");
+
+
+            //10 interface inheritance
+            //A a=new A();
+            //a.Drive();
+            //Console.Write("\n");
+            //a.race();
+
+            //11.is-a relationship
+            //Animal animal = new Animal();
+            //animal.Name = "Puppy";
+            //animal.Eat();
+            //Console.WriteLine("\n");
+
+            //Dog dog = new Dog();
+            //dog.Name = "German";
+            //dog.Eat();
+            //dog.Bark();
+
+            //12.HAS 
+            //Engine e = new Engine(200,"Honda");
+            //Console.WriteLine("\n");
+            //Car c = new Car("Benz", 450, "Petrol-Engine");
+
+            //13.base method
+
+            //Person p = new Person("Norah", "engineer");
+            //p.DisplayInfo();
+
+            //Console.WriteLine("\n");
+
+            //Employee E = new Employee("Martin","Doctor",23);
+            //E.DisplayInfo();
+
+            //14. base
+
+            //Person P = new Person("Norah");
+
+            //Console.WriteLine("\n");
+            //Student S = new Student("Martin");
+
+
+            //15.
+            Vehicle V = new Vehicle("Benz");
+            Console.WriteLine("\n");
+            Car c = new Car("BMW");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
