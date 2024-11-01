eaa2be908a2021ff8584577ec38d430695e57d02
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-11-01 16:44:22 +0530

    Assignment-11 uploaded
    Question on try...catch, finally, Exceptions, ref

---
+++ Assignment11/Assignment11/Class1.cs
@@ -0,0 +1,272 @@
+﻿using Assignment11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+using System.Runtime.Remoting.Messaging;
+using System.CodeDom;
+namespace Assignment11
+{
+    //    //1.Create a Library class to store and manage books in a single-dimensional array where
+    //   // each element represents a book title
+    //public class Library
+    //{
+    //    public string[] Books = new string[3];
+    //    public void Display()
+    //    {
+    //        int i = 0;
+    //        //loop to display the books
+    //        foreach (var item in Books)
+    //        {
+
+    //            Console.WriteLine($"{i}  --   {item}");
+    //            Console.WriteLine("\n");
+    //            i++;
+    //        }
+    //    }
+
+    //}
+    //         //    use a two-dimensional array in a Classroom class
+    //        //where each element represents the seat of a student based on rows and columns.
+
+    //public class Classroom
+    //{
+    //    public string[,] Seats = new string[2, 2];
+    //    public void Display()
+    //    {
+    //        for (int i = 0; i < Seats.GetLength(0); i++)
+    //        {
+    //            for (int j = 0; j < Seats.GetLength(1); j++)
+    //            {
+    //                Console.WriteLine($"Seat:{i}-{j}    -- {Seats[i, j]} ");
+    //                Console.WriteLine("\n");
+    //            }
+
+    //        }
+
+    //    }
+    //}
+
+
+    //2.array-Define a Movie class with properties like Title and Year. Create a MovieLibrary class
+    // that stores an array of Movie objects and displays each movie’s information
+    //public class Movie
+    //{
+    //    public string Title { get; set; }
+    //    public string Year { get; set; }
+    //    public Movie(string title, string year)
+    //    {
+    //        Title = title;
+    //        Year = year;
+    //    }
+
+    //}
+    //public class MovieLibrary
+    //{
+    //    //array to hold object,instaed of string or int we give class name
+
+    //    public Movie[] Movies = new Movie[2];
+
+    //    public void MovieAdd(Movie movie, int index)
+    //    {
+
+    //        for (int j = index; j < 2; j++)
+    //        {
+    //            Movies[j] = movie;
+
+    //        }
+    //    }
+    //    public void Display()
+    //    {
+    //        for (int j = 0; j < 2; j++)
+
+    //        {
+    //            Console.WriteLine($"{Movies[j].Title}   --  {Movies[j].Year}");
+    //            Console.WriteLine("\n");
+    //        }
+    //    }
+
+    //}
+
+    //3.Create an enumeration OrderStatus with values Pending, Shipped, and Delivered.
+    //Define an Order class with an OrderStatus property to manage the status of each order
+
+    //public class Order
+    //{
+    //    public string Name;
+    //    //ENUMERATION
+    //    public OrderStatus Status;
+    //    public Order(string name, OrderStatus status)
+    //    {
+    //        Name = name;
+    //        Status = status;
+    //    }
+    //    //ARRAY TO STORE MULTIPLE ORDERS
+
+    //    public Order[] Orders = new Order[4];
+    //    public enum OrderStatus
+    //    {
+    //        Pending,
+    //        Shipped,
+    //        Delivered
+    //    }
+    //    public void AddOrder(Order ord, int index)
+    //    {
+    //        for (int i = index; i < Orders.Length; i++)
+    //        {
+    //            Orders[i] = ord;
+    //        }
+    //    }
+
+
+    //    public void Display()
+    //    {
+    //        Console.WriteLine("Orders.Name  ----      OrdersStatus---   OrdersStatusId");
+    //        Console.WriteLine("\n");
+
+    //        for (int i = 0; i < Orders.Length; i++)
+    //        {
+    //            Console.WriteLine($"{Orders[i].Name}\t  {Orders[i].Status}\t\t  {(int)Orders[i].Status}");
+    //            Console.WriteLine("\n");
+    //        }
+    //    }
+    //}
+
+
+    //4.struct--Create a Circle struct to represent a circle as a value type, and a Shape class to
+    //represent a reference type.Show how changing values affects each.
+
+    //struct Circle
+    //{
+    //    public int radius;
+    //    public void Calculate(int radius)
+    //    {
+    //        Console.WriteLine($"area is:{3.14 * radius * radius}");
+
+    //        radius = 4;
+
+
+
+    //    }
+    //}
+    //public class Shape
+    //{
+    //    public int radius;
+    //    public void Calculate(int radius)
+    //    {
+    //        Console.WriteLine($"area is:{3.14 * radius * radius}");
+
+    //        radius = 4;
+    //    }
+
+    //}
+
+    //12.user defined exception
+
+    //public class NegativeNumberException : Exception
+    //{
+
+    //    public NegativeNumberException()
+    //    {
+    //        Console.WriteLine("Number less than zerooo");
+
+
+    //    }
+    //}
+
+
+    //14..user defined
+    //public class NegativeNumberException : Exception
+    //{
+    //    public NegativeNumberException()
+    //    {
+    //        Console.WriteLine("less than zerooooo");
+    //    }
+
+    //}
+
+
+    //15  a
+    //    Create a class FileProcessor with a method ReadFileContent(string filePath).
+    //o In this method, attempt to read content from the file located at filePath using
+    //StreamReader.
+    //o Handle FileNotFoundException by printing a message that says "File not found:
+    //<filePath>". 
+
+
+    //public class FileProcessor
+    //{
+    //    public void ReadFileContent(string filePath)
+    //    {
+    //        StreamReader sr = new StreamReader(filePath);
+    //        string text = sr.ReadToEnd();
+    //        Console.WriteLine(text);
+
+    //        //closing file
+    //        sr.Close();
+    //    }
+    //}
+
+
+    //15b  Create a class MathOperations with a method Divide(int a, int b).
+    //o This method should return the result of dividing a by b.
+    //o Handle DivideByZeroException and print "Cannot divide by zero"
+
+    //public class MathOperations
+    //{
+    //    public int Divide(int a, int b)
+    //    {
+    //        if (b == 0)
+    //        {
+    //            throw new DivideByZeroException();
+    //        }
+    //        return (a / b);
+    //    }
+
+    //}
+
+
+    //15c Create a custom exception called InvalidAgeException that inherits from
+    //    Exception.
+    //    o Create a class Person with a property Age.
+    //    o Add a method SetAge(int age) in Person that throws an InvalidAgeException if
+    //age is less than 0 or greater than 120.
+    //o Print "Invalid age provided" if the exception is thrown.
+
+    //public class Person
+    //{
+    //    public int age;
+    //    public void SetAge(int age)
+    //    {
+    //        if (age < 0 || age > 120)
+    //        {
+    //            Console.WriteLine("\n");
+    //            throw new InvalidAgeException();
+    //        }
+    //        Console.WriteLine($"age is : {age}");
+    //    }
+    //}
+
+    ////exception
+    //public class InvalidAgeException : Exception
+    //{
+    //    public InvalidAgeException()
+    //    {
+    //        Console.WriteLine("Invalid age is provided");
+
+    //    }
+
+    //}
+
+
+    //15d.
+}
+
+
+
+
+
---
+++ Assignment11/Assignment11/Program.cs
@@ -0,0 +1,471 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.Common;
+using System.Runtime.Remoting.Messaging;
+using System.CodeDom;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1. library and array declaration
+            //try
+            //{
+            //    Library library = new Library();
+            //    library.Books[0] = "I am Malala";
+            //    library.Books[1] = "The kite Runner";
+            //    library.Books[2] = "Born a Crime";
+            //    library.Display();
+
+            //    Console.WriteLine("--------------------");
+            //    Classroom classroom = new Classroom();
+            //    classroom.Seats[0, 0] = "Norah";
+            //    classroom.Seats[0, 1] = "Martin";
+            //    classroom.Seats[1, 0] = "Isabella";
+            //    classroom.Seats[1, 1] = "Edvin";
+
+            //    classroom.Display();
+            //    //to show the error
+            //    classroom.Seats[2, 3] = "xxxxx";
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Source);
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("ERROR");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("---------------end-----------------");
+            //}
+
+
+
+            ////2.movie array
+            //try
+            //{
+            //    Movie movie1 = new Movie("JanaGanaMana", "2021");
+            //    Movie movie2 = new Movie("SpderMAN", "2008");
+            //    MovieLibrary movieLibrary = new MovieLibrary();
+            //    movieLibrary.MovieAdd(movie1, 0);
+            //    movieLibrary.MovieAdd(movie2, 1);
+
+            //    movieLibrary.Display();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Source);
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("ERROR");
+
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("-----------end-------------");
+            //}
+
+
+            //3.order status
+            //Order order = new Order("Keyboard", Order.OrderStatus.Pending);
+            //Order order1 = new Order("Mouse", Order.OrderStatus.Shipped);
+            //Order order2 = new Order("Phone", Order.OrderStatus.Pending);
+            //Order order3 = new Order("Bottle", Order.OrderStatus.Delivered);
+            //order.AddOrder(order, 0);
+            //order.AddOrder(order1, 1);
+            //order.AddOrder(order2, 2);
+            //order.AddOrder(order3, 3);
+
+            //order.Display();
+
+            ////4.struct and circle
+            //try
+            //{
+            //    Circle circle1 = new Circle();
+            //    circle1.radius = 10;
+            //    Circle circle2 = new Circle();
+
+            //    Console.WriteLine($"value before:{circle1.radius}");
+            //    Console.WriteLine($"value before:{circle2.radius}");
+            //    circle1.radius = 5;
+
+            //   // circle1.Calculate(circle1.radius);
+
+            //    Console.WriteLine($"Value After :{circle1.radius}");
+            //    Console.WriteLine($"value before:{circle2.radius}");
+
+            //    Console.WriteLine("\n");
+            //    Shape shape = new Shape();
+            //    shape.radius = 10;
+            //    Console.WriteLine($"value before:{shape.radius}");
+            //    shape.Calculate( shape.radius);
+            //    Console.WriteLine($"Value After :{shape.radius}");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Source);
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("ERROR");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("-----------the end-----------");
+            //}
+
+            //5.. Create a method IncreaseScore that takes a ref integer parameter representing a
+            //player’s score and increases it by 10.
+
+            //int Score = 4;
+            //Console.WriteLine($"Score Before:{Score}");
+            //IncreaseScore(ref Score);
+            //Console.WriteLine("\n");
+            //Console.WriteLine($"Score After:{Score}");
+            ////function
+            //void IncreaseScore(ref int num)
+            //{
+            //    Score = num + 10;
+
+            //}
+
+
+            //6.Create a method Divide that takes two integers, calculates their quotient and
+            // remainder, and returns both values using the out keyword.
+
+            //int num1 = 20; //just to show the difference before this
+            //int num2 = 30;
+            //Console.WriteLine($"number1 is {num1} and num2:  {num2}");
+            //Divide(out num1, out num2);
+            //Console.WriteLine("\n");
+            //Console.WriteLine($"number1 is {num1} and num2:  {num2}");
+
+            //void Divide(out int number1, out int number2)
+            //{
+            //    number1 = 1000; //this value used
+            //    number2 = 100;
+            //    if (number2 == 0)
+            //    {
+            //        Console.WriteLine("no division");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"division is:{number1 / number2}");
+            //    }
+
+            //}
+
+            //7.Write a C# program that demonstrates a FileNotFoundException and an
+            // IndexOutOfRangeException.Explain what causes these exceptions.
+
+            ////FILE NOT FOUND EXCEPTION
+            //try
+            //{
+            //    using (StreamReader read = new StreamReader("demo.txt")) //using --automatic closing
+            //    {
+            //        read.ReadToEnd();
+            //    }
+
+            //}
+            //catch (FileNotFoundException ex)
+            //{
+            //    Console.WriteLine("--error--");
+            //    Console.WriteLine(ex.Source);
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+
+            //}
+
+            ////index out of range
+            //try
+            //{
+            //    int[] arr = { 1, 2, 3 };
+            //    Console.WriteLine($"{arr[0]}");
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine($"{arr[5]}"); //error
+            //}
+            //catch (IndexOutOfRangeException ex)
+            //{
+            //    Console.WriteLine("--error--");
+
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.Message);
+
+            //}
+
+            //8.Question: Create a method GetUserInput that prompts the user to enter an integer. If the input is
+            //  invalid, catch the exception and prompt the user again until they enter a valid integer.
+
+            //GetUserInput();
+            //void GetUserInput()
+            //{
+            //    int number;
+            //    //to loop till we get number
+            //    while (true)
+            //    {
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine("enter the number:");
+            //        try
+            //        {
+            //            number = int.Parse(Console.ReadLine());
+            //            Console.WriteLine($"entered number: {number}");
+            //            break;
+
+            //        }
+            //        catch (FormatException ex) //if not integer //small letter
+            //        {
+
+            //            Console.WriteLine(ex.Message);
+            //        }
+            //    }
+
+            //}
+
+            //9.Create a method that calculates the division of two integers. Use multiple catch blocks
+            //to handle DivideByZeroException and FormatException.
+            //try
+            //{
+            //    //get the input of two numbers;
+            //    Console.Write("enter the first number: ");
+            //    int number1 = int.Parse(Console.ReadLine());
+
+            //    Console.Write("\n enter the second number: ");
+            //    int number2 = int.Parse(Console.ReadLine());
+
+            //    //check second number zero?
+            //    if (number2 == 0)
+            //    {
+            //        throw new DivideByZeroException();
+            //    }
+            //    Console.Write($"quotient is: {number1 / number2}");
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine("------");
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("\nend---------");
+            //}
+
+            //10.  Write a method ReadFile that opens a file, reads its contents, and closes it. Use a
+            //finally block to ensure the file is closed, even if an exception occurs.
+            //ReadFile();
+            //void ReadFile()
+            //{//reading a file
+
+            //    try
+            //    {
+            //        StreamReader reader = new StreamReader("C:/Users/ASUS/Desktop/c#.net/sample.txt");
+            //        string text = reader.ReadToEnd();
+            //        Console.WriteLine(text);
+
+            //        reader.Close();
+            //    }
+            //    catch (FileNotFoundException ex)
+            //    {
+            //        Console.WriteLine("File not found.");
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine(ex.StackTrace);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("An error occurred.");
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine(ex.StackTrace);
+            //    }
+            //    finally
+            //    {
+            //        // Ensure the file is closed, even if an exception occurred
+
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine("File closed.");
+
+
+
+            //          }
+
+            //        }
+
+
+            //11 Create a method ValidateAge that
+            // throws an exception if the age is below 18, and handle it in the calling method.
+            //ValidateAge();
+            //void ValidateAge()
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("ENTER THE AGE: ");
+            //        int age = int.Parse(Console.ReadLine());
+
+            //        if (age < 18)
+            //        {
+            //            throw new ArgumentException();
+
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"age is:{age}");
+            //        }
+            //    }
+            //    catch (ArgumentException ex)
+            //    {
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine(ex.StackTrace);
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine("------error------");
+            //    }
+            //    finally
+            //    {
+            //        Console.WriteLine("------end-------");
+            //    }
+
+            //}
+
+            //12. Create a custom exception class NegativeNumberException to handle negative inputs.
+            //Write a method CheckNumber that throws this exception if the number is negative.
+
+            //try
+            //{
+            //    Console.WriteLine("enter the number");
+            //    int number = int.Parse(Console.ReadLine());
+            //    CheckNumber(number);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("--error--");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("----end-----");
+            //}
+            //void CheckNumber(int number)
+            //{
+            //    if (number < 0)
+            //    {
+            //        throw new NegativeNumberException();
+            //    }
+            //    Console.WriteLine($"number is : {number}");
+
+            //}
+
+            //14.. Create a custom exception NegativeNumberException to handle scenarios where a
+            //negative number is passed to a method that only accepts positive numbers.Write code to throw
+            // this exception if the number is negative.
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine("ENTER THE NUMBER: ");
+            //        int number = int.Parse(Console.ReadLine());
+            //        CheckNumber(number);
+            //        break;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine(ex.StackTrace);
+            //        Console.WriteLine("--error--");
+            //    }
+            //    finally
+            //    {
+
+            //        Console.WriteLine("\n");
+            //        Console.WriteLine("---------END------------");
+            //    }
+            //}
+            //    void CheckNumber(int number)
+            //    {
+            //        if(number<0)
+            //        {
+            //            throw new NegativeNumberException();
+            //        }
+            //        Console.WriteLine($"number is:{number}");
+
+            //    }
+
+
+
+            //15.a fileproceesor
+            //try
+            //{
+            //    FileProcessor file = new FileProcessor();
+            //    file.ReadFileContent("C:/Users/ASUS/Desktop/c#.net/sample.txt");
+
+
+            //}
+            //catch (FileNotFoundException ex)
+            //{
+            //    Console.WriteLine("FILE NOT FOUND");
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("----END-----");
+            //}
+
+
+            //15b mathoperations
+            //try
+            //{
+            //    MathOperations maths = new MathOperations();
+            //    int ans = maths.Divide(8, 2);
+            //    Console.WriteLine($"quotient is: {ans}");
+            //    int res = maths.Divide(8, 0);
+
+
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine("cannot divide by zero..");
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+
+            //15c custom invalid exception
+            //try
+            //{
+            //    Person person = new Person();
+            //    person.SetAge(24);
+            //    person.SetAge(150);
+            //}
+            //catch (InvalidAgeException ex)
+            //{
+            //    Console.WriteLine("\n");
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
