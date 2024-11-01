aa4f0acef3cdc6e01f87fc30d57fb22c92cc4387
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2024-11-01 16:46:29 +0530

    Assignment 11
    Assignment 11-log4net

---
+++ Assignment11/Assignment11/log4net/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+
+namespace log4net
+{
+    public class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+        static Logger()
+        {
+            XmlConfigurator.Configure();
+        }
+        public static void LogError(string message, Exception ex)
+        {
+            Console.WriteLine("\n");
+            log.Error(message, ex);
+            log.Info(message, ex);
+            log.Fatal(message, ex);
+            log.Debug(message, ex);
+            log.Warn(message, ex);
+            
+
+        }
+    }
+}
---
+++ Assignment11/Assignment11/log4net/Program.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace log4net
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//        }
+//    }
+//}
+using log4net;
+using System;
+public class LoggingExample
+{
+    public static void Divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Logger.LogError("Attempted to divide by zero.", ex);
+           
+
+            Console.WriteLine("\n");
+            Console.WriteLine("DivideByZeroException caught and logged.");
+        }
+    }
+    public static void Main()
+    {
+        Divide(10, 0);
+        Console.ReadLine();
+    }
+}
